f1288eec624bc9c3e22f56e1e9251cb943071dba
Timo Michna <timomichna@yahoo.de>
2017-04-16 14:52:26 +0200

    add 'behavior:implement' config

--- spec/Command/ImplementCommandSpec.php
+++ spec/Command/ImplementCommandSpec.php
@@ -11,13 +11,25 @@ namespace spec\Tidal\PhpSpec\Behavior\Command;
 use Tidal\PhpSpec\Behavior\Command\ImplementCommand;
 use PhpSpec\ObjectBehavior;
 use Symfony\Component\Console\Command\Command;
-use Prophecy\Argument;
+use Symfony\Component\Console\Input\InputDefinition;
 
 /**
-* class spec\Tidal\PhpSpec\Behavior\Command\ImplementCommandSpec
-*/
+ * class spec\Tidal\PhpSpec\Behavior\Command\ImplementCommandSpec
+ *
+ * @method ImplementCommandSpec getName()
+ * @method ImplementCommandSpec getDescription()
+ * @method ImplementCommandSpec getHelp()
+ * @method ImplementCommandSpec getDefinition()
+ */
 class ImplementCommandSpec extends ObjectBehavior
 {
+
+    private const CONFIG = [
+        'NAME' => ImplementCommand::NAME,
+        'DESCRIPTION' => ImplementCommand::DESCRIPTION,
+        'HELP' => ImplementCommand::HELP
+    ];
+
 
     function it_is_initializable()
     {
@@ -31,6 +43,57 @@ class ImplementCommandSpec extends ObjectBehavior
 
     function it_has_a_name()
     {
-        $this->getName()->shouldReturn(ImplementCommand::NAME);
+        $this->getName()->shouldReturnConstant('NAME');
+    }
+
+    function it_has_a_description()
+    {
+        $this->getDescription()->shouldReturnConstant('DESCRIPTION');
+    }
+
+    function it_has_help()
+    {
+        $this->getHelp()->shouldReturnConstant('HELP');
+    }
+
+    function it_has_arguments()
+    {
+        foreach (ImplementCommand::ARGUMENTS as $name => $config) {
+            $this->getDefinition()->shouldHaveConsoleArgument($name);
+        }
+    }
+
+    function it_has_options()
+    {
+        foreach (ImplementCommand::OPTIONS as $name => $config) {
+            $this->getDefinition()->shouldHaveConsoleOption($name);
+        }
+    }
+
+    private function shouldReturnConstant(string $value, string $key = ''): bool
+    {
+        return array_key_exists($key, self::CONFIG) ?? self::CONFIG[$key] === $value;
+    }
+
+    private function shouldHaveConsoleArgument(InputDefinition $inputDefinition, $name = ''): bool
+    {
+        return $inputDefinition->hasArgument($name);
+    }
+
+    private function shouldHaveConsoleOption(InputDefinition $inputDefinition, $name = ''): bool
+    {
+        return $inputDefinition->hasOption($name);
+    }
+
+    /**
+     * @return callable[]
+     */
+    public function getMatchers()
+    {
+        return [
+            'returnConstant' => \Closure::fromCallable([$this, 'shouldReturnConstant']),
+            'haveConsoleArgument' => \Closure::fromCallable([$this, 'shouldHaveConsoleArgument']),
+            'haveConsoleOption' => \Closure::fromCallable([$this, 'shouldHaveConsoleOption']),
+        ];
     }
 }
--- src/Command/ImplementCommand.php
+++ src/Command/ImplementCommand.php
@@ -11,6 +11,8 @@
 namespace Tidal\PhpSpec\Behavior\Command;
 
 use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Input\InputArgument;
+use Symfony\Component\Console\Input\InputOption;
 
 /**
  * class Tidal\PhpSpec\Behavior\Command\ImplementCommand
@@ -18,9 +20,77 @@ use Symfony\Component\Console\Command\Command;
 class ImplementCommand extends Command
 {
     public const NAME = 'behavior:implement';
+    public const DESCRIPTION = 'Creates a Trait from a given Interface';
+    public const HELP = <<<EOF
+The <info>%command.name%</info> command creates an trait from a given interface:
+  <info>php %command.full_name% ClassName MethodName</info>
+Will generate an example in the ClassNameSpec.
+EOF;
+
+    private const INTERFACE_INPUT = 'interface';
+    private const TRAIT_INPUT = 'trait';
+
+    private const MODE_KEY = 'mode';
+    private const DESCRIPTION_KEY = 'description';
+    private const CONFIRM_KEY = 'confirm';
+
+    public const ARGUMENTS = [
+        self::INTERFACE_INPUT => [
+            self::MODE_KEY => InputArgument::REQUIRED,
+            self::DESCRIPTION_KEY => 'Interface to create behavior for'
+        ],
+        self::TRAIT_INPUT => [
+            self::MODE_KEY => InputArgument::OPTIONAL,
+            self::DESCRIPTION_KEY => 'Custom trait class name'
+        ]
+    ];
+
+    public const OPTIONS = [
+        self::CONFIRM_KEY => [
+            self::MODE_KEY => InputOption::VALUE_NONE,
+            self::DESCRIPTION_KEY => 'Ask for confirmation before creating trait'
+        ]
+    ];
 
     public function __construct()
     {
         parent::__construct(self::NAME);
     }
+
+    protected function configure()
+    {
+        $this
+            ->setName(self::NAME)
+            ->setDescription(self::DESCRIPTION)
+            ->setDefinition(
+                $this->createArguments()
+            )
+            ->addOption(
+                self::CONFIRM_KEY,
+                null,
+                self::OPTIONS[self::CONFIRM_KEY][self::MODE_KEY],
+                self::OPTIONS[self::CONFIRM_KEY][self::DESCRIPTION_KEY]
+            )
+            ->setHelp(
+                self::HELP
+            );
+    }
+
+    /**
+     * @return InputArgument[]
+     */
+    private function createArguments()
+    {
+        $arguments = [];
+        foreach (self::ARGUMENTS as $name => $argument) {
+            $arguments[] = new InputArgument(
+                $name,
+                $argument[self::MODE_KEY],
+                $argument[self::DESCRIPTION_KEY]
+            );
+        }
+
+        return $arguments;
+    }
 }
+
