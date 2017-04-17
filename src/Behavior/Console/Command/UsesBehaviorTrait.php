0fada77f05b05a93d76add4e463b286031fd945c
Timo Michna <timomichna@yahoo.de>
2017-04-17 23:26:39 +0200

    add UsesBehaviorTrait

---
+++ src/Behavior/Console/Command/UsesBehaviorTrait.php
@@ -0,0 +1,25 @@
+<?php
+/**
+ * This file is part of the phpspec-behavior package.
+ * (c) 2017 Timo Michna <timomichna/yahoo.de>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Tidal\PhpSpec\BehaviorExtension\Behavior\Console\Command;
+
+/**
+ * trait Tidal\PhpSpec\BehaviorExtension\Behavior\Console\Command\UsesBehaviorTrait
+ */
+trait UsesBehaviorTrait
+{
+    /**
+     * @param string $traitName
+     * @return bool
+     */
+    public function validateTrait(?string $traitName): bool
+    {
+        return trait_exists($traitName);
+    }
+}
