2da64811d6343dc16a152e0ac073f2b673385c3b
Timo Michna <timomichna@yahoo.de>
2017-04-17 23:26:04 +0200

    add UsesInterfaceTrait

---
+++ src/Behavior/Console/Command/UsesInterfaceTrait.php
@@ -0,0 +1,65 @@
+<?php
+/**
+ * This file is part of the phpspec-behavior package.
+ * (c) 2017 Timo Michna <timomichna/yahoo.de>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Tidal\PhpSpec\BehaviorExtension\Behavior\Console\Command;
+
+use Throwable;
+
+use Tidal\PhpSpec\BehaviorExtension\Exception\NoInterfaceException;
+use Tidal\PhpSpec\ConsoleExtension\Contract\WriterInterface;
+
+/**
+ * trait Tidal\PhpSpec\BehaviorExtension\Behavior\Console\Command\UsesInterfaceTrait
+ */
+trait UsesInterfaceTrait
+{
+    /**
+     * @param string $interfaceName
+     * @return bool
+     */
+    public function validateInterface(string $interfaceName)
+    {
+        return interface_exists($interfaceName);
+    }
+
+    /**
+     * @param string $interfaceName
+     */
+    public function requireInterface(string $interfaceName)
+    {
+        if (!$this->validateInterface($interfaceName)) {
+            throw new NoInterfaceException($interfaceName);
+        }
+    }
+
+    /**
+     * @param string $interfaceName
+     * @throws Throwable
+     */
+    public function demandInterface(string $interfaceName)
+    {
+        try {
+            $this->requireInterface($interfaceName);
+        } catch (Throwable $exception) {
+            $this->getWriter()->writeError($exception->getMessage());
+
+            throw $exception;
+        }
+    }
+
+    /**
+     * @param WriterInterface $writer
+     */
+    abstract public function setWriter(WriterInterface $writer);
+
+    /**
+     * @return WriterInterface
+     */
+    abstract public function getWriter(): WriterInterface;
+}
